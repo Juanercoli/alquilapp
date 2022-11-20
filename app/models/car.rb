82b012dc69db618f26bd75307238b60521ab4f72
Juan M. Ercoli <ercolijuan@yahoo.com.ar>
2022-11-20 17:47:49 -0300

    Valida que la patente sea argentina como messi

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,11 +4,8 @@ class Car < ApplicationRecord
   # Validaciones a nivel base de datos
   validates :brand, presence: true
   validates :color, presence: true
-  validates :patent, presence: true, uniqueness: true,
-    format: {
-      with: /\A([A-Z]{2,3}[0-9]{3}[A-Z]{0,2})?\z/,
-      message: 'tiene que ser en formato AAA111 o AA11AA'
-  }
+  validates :patent, presence: true, uniqueness: true
+  validate :validate_patent_argentina
   validates :vehicle_number, presence: true, uniqueness: true
   validates :model, presence: true
   validates :photo, presence: true
@@ -31,5 +28,16 @@ class Car < ApplicationRecord
     if photo.attached? && !photo.content_type.in?(%w(image/jpeg image/png))
       errors.add(:photo, :bad_photo_type, type1: ".jpg", type2: ".png")
     end
+  end
+
+  def validate_patent_argentina
+    # 3 letras y 3 números
+    old_patent_regex = /\A[a-zA-Z]{3}[\d]{3}\z/
+    # 2 letras, 3 números y 2 letras
+    new_patent_regex = /\A[a-zA-Z]{2}[\d]{3}[a-zA-Z]{2}\z/
+    # Si el formato es válido hay match
+    is_valid = patent.match(old_patent_regex) || patent.match(new_patent_regex)
+
+    is_valid ? true : errors.add(:patent, :bad_patent_type)
   end
 end
